ed1d523364b6ccc24f16d54baf049187ecf5c00a
Bil4Ben <hushed.names@mail.com>
2022-10-17 23:15:42 +0000

    Commit: c code tells a # is  +ve, -ve or zero

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Check numbers polarity
+* Pos, neg, or zero (comparison statment)
+* Return: noting
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+		printf("%i is positive\n", n);
+	else if (n > 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero \n", n);
+return (0);
+}
