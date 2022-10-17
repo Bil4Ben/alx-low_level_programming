01a36fb5fc482867636b3645c5298e33da75fc8e
Bil4Ben <hushed.names@mail.com>
2022-10-17 20:57:17 +0000

    Commit: C code displays text on standard output using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Prints out strings using printf
+* function
+* Return: Nothing
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art \n");
+return (0);
+}
