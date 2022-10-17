a62a9c749f2af92fed5c16a2645ce96300b5568e
Bil4Ben <hushed.names@mail.com>
2022-10-17 20:39:49 +0000

    Commit: C code displays text on standard output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h> /*The header file*/
 /**
-* main - Displays given text
-* on a standard output.
+* main - Prints out programming
+* puts function is used to print out the string to
+* the stdio library displays the string passed as 
+* an argument for the puts function.
+* Return: All good
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
