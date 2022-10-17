18e58845c5f1e5f25a5f3489fc3971140933fa3b
Bil4Ben <hushed.names@mail.com>
2022-10-17 22:27:50 +0000

    Commit: C program outputs Memory size of data types.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
 * floating int data types. By the way alx
 * training has some ways that are not
 * logical. are we learning programming or
-* copying? There must be freedom for
+* copying? There must be freedom and logic for
 * programming and software enginnering.
 * Return: Nothing
 */
 void main(void)
 {
-	printf("Size of char: %li byte(s)\n", sizeof(char));
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
 	printf("Size of an int: %li byte(s)\n", sizeof(int));
-	printf("Size of long int: %li byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %li byte(s)\n", sizeof(long long int));
-	printf("Size of float: %li byte(s)\n", sizeof(float));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
