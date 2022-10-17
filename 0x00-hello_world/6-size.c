d6df06498a455a49f298b8cf670b0c5305fd8aac
Bil4Ben <hushed.names@mail.com>
2022-10-17 22:40:43 +0000

    Commit: C program outputs Memory size of data types.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
+#nclude <stdio.h>
 
 /**
-* main - It lists out built in data type size
-* char, int, long int, long long int, and
-* floating int data types. By the way alx
-* training has some ways that are not
-* logical. are we learning programming or
-* copying? There must be freedom and logic for
-* programming and software enginnering.
-* Return: Nothing
+* main - Prints out the size of c datatypes
+* Return: All good
 */
-void main(void)
+
+int main(void)
 {
 	printf("Size of a char: %li byte(s)\n", sizeof(char));
 	printf("Size of an int: %li byte(s)\n", sizeof(int));
@@ -23,9 +18,9 @@ void main(void)
 /* Note!
 * You should get something like this as an output
 * which are the sizes of the built in data types in C
-Size of a char: 1 byte(s) which is 8 bits, example is letter a
-Size of an int: 4 byte(s)
-Size of a long int: 8 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
- */
+* Size of a char: 1 byte(s) which is 8 bits, example is letter a
+* Size of an int: 4 byte(s)
+* Size of a long int: 8 byte(s)
+* Size of a long long int: 8 byte(s)
+* Size of a float: 4 byte(s)
+*/
