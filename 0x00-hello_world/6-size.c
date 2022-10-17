1b9af082d66ea2a55c6cfe498b34047d11d38c7b
Bil4Ben <hushed.names@mail.com>
2022-10-17 22:36:20 +0000

    Commit: C program outputs Memory size of data types.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,3 +19,13 @@ void main(void)
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
+
+/* Note!
+* You should get something like this as an output
+* which are the sizes of the built in data types in C
+Size of a char: 1 byte(s) which is 8 bits, example is letter a
+Size of an int: 4 byte(s)
+Size of a long int: 8 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
+ */
