a7e18ad5faf988f50648d81544ba47e3d204b39f
Bil4Ben <hushed.names@mail.com>
2022-10-17 22:21:11 +0000

    Commit: C program outputs Memory size of data types.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,19 @@
 /**
 * main - It lists out built in data type size
 * char, int, long int, long long int, and
-* floating int data types
+* floating int data types. By the way alx
+* training has some ways that are not
+* logical. are we learning programming or
+* copying? There must be freedom for 
+* programming and software enginnering.
 * Return: Nothing
 */
 void main(void)
 {
 	printf("Size of char: %li byte(s)\n", sizeof(char));
-	printf("Size of int: %li bytes(s)\n", sizeof(int));
-	printf("Size of long int: %li bytes(s)\n", sizeof(long int));
-	printf("Size of long long int: %li bytes(s)\n", sizeof(long long int));
-	printf("Size of float: %li bytes(s)\n", sizeof(float));
+	printf("Size of int: %li byte(s)\n", sizeof(int));
+	printf("Size of long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
