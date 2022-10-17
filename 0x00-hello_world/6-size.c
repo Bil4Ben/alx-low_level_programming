f5c9a91704441f49afc5cdb4481f3059863a0119
Bil4Ben <hushed.names@mail.com>
2022-10-17 22:07:47 +0000

    Commit: C program outputs Memory size of data types.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
 * main - It lists out built in data type size
-* char, short int, unsigned int, byte, floating
-* unsigned long int data types
+* char, int, long int, long long int, and
+* floating int data types
 * Return: Nothing
 */
 void main(void)
@@ -12,5 +13,5 @@ void main(void)
 	printf("Memory size of long int:%li bytes\n", sizeof(long int));
 	printf("Memory size long long int:%li bytes\n", sizeof(long long int));
 	printf("Memory size of float:%li bytes\n", sizeof(float));
-return (0);
+	return (0);
 }
