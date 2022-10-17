acd42e9f5d6bd272c4b7db73ff09de9ac3303470
Bil4Ben <hushed.names@mail.com>
2022-10-17 20:45:42 +0000

    Commit: C code displays text on standard output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
 * main - Prints out programming
 * puts function is used to print out the string to
-* the stdio library displays the string passed as 
+* the stdio library displays the string passed as
 * an argument for the puts function.
 * Return: All good
 */
