b24f90d2b30b54f526a6a5ad6726d06540eb3586
Bil4Ben <hushed.names@mail.com>
2022-10-18 00:55:11 +0000

    Commit: c code convert all letters into lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,17 +6,19 @@
 /**
 * main - converts upper case letters
 * into lower case using string library.
+* No other way of writing this code
+* doesn't work in alx. Really amazing!!!
 * Return: nothing
 */
 
 int main(void)
 {
-	char *str = "Alx MUST CONSIDER PROGRAMMER RIGHT";
-	int i = 0;
+	char a;
 
-	for (i; i < strlen(str); ++i)
-	printf("%c", tolower((unsigned char) str[i]));
-	printf("\n");
-
+	for (a = 'a'; a < = 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
 return (0);
 }
