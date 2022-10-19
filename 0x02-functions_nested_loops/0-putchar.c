f38c7c69ae4ce3e1c4327a49cb7c337f5a398149
Bil4Ben <hushed.names@mail.com>
2022-10-19 01:50:27 +0000

    Commit: C code prints text assigned to char variable

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+/**
+* main - Prints "_putchar" characters
+* on standard output
+* Return: Noting
+*/
+int main(void)
+{
+	char *s = "_putchar";
+	for (int i = 0; i < 8; i++)
+		{
+		_putchar(s[i]);
+		}
+	_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+C coding practice of nested loops. \n
